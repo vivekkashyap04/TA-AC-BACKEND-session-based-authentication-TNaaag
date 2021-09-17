fa494549109df4842946e18fd3bec93759a1b0dc
vivek <kumarvivek313@gmail.comgit>
2021-09-17 11:54:17 +0530

    A-01

---
+++ block-BNaadv/user-login/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, require: true, minlength: 5 },
+  age: { type: Number },
+  phone: Number,
+});
+
+userSchema.pre('save', function (next) {
+  if (this.password && this.isModified('password')) {
+    bcrypt.hash(this.password, 10, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      return next();
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.verifyPassword = function (password, cb) {
+  bcrypt.compare(password, this.password, (err, result) => {
+    return cb(err, result);
+  });
+};
+
+module.exports = mongoose.model('User', userSchema);
